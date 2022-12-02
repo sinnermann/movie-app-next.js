98241abbb2bf3607c3a21311c03dd01ee7d7a01a
sinnerman <sinnerman@gmail.com>
2022-12-02 19:23:06 +0400

    placing navbar component & configuring requests.js file

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,23 @@
 import React from "react";
+import requests from "../utils/requests";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <h2
+          onClick={() => router.push(`?genre=${key}`)}
+          className="m-6 cursor-pointer hover:text-white active:text-yellow-300"
+          key={key}
+        >
+          {title}
+        </h2>
+      ))}
+    </div>
+  );
 };
 
 export default Navbar;
