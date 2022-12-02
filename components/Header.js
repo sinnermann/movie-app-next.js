c9e3b9353d9a9201bc7c0242c9a1b6fbed3bcf57
sinnerman <sinnerman@gmail.com>
2022-12-02 18:45:19 +0400

    completion of the header section

--- components/Header.js
+++ components/Header.js
@@ -10,14 +10,19 @@ import {
 
 const Header = () => {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
         <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
       </div>
-      <Image src="/IMDB_Logo_2016.svg" width={100} height={100} />
+      <Image
+        src="/IMDB_Logo_2016.svg"
+        width={100}
+        height={100}
+        className="cursor-pointer active:brightness-110"
+      />
     </div>
   );
 };
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const HeaderIcon = ({ Icon, title }) => {
   return (
-    <div>
+    <div className="mx-4 flex flex-col cursor-pointer hover:text-white active:text-yellow-400 lg:mx-6">
       <Icon className="h-8" />
-      <p>{title}</p>
+      <p className="my-2">{title}</p>
     </div>
   );
 };
