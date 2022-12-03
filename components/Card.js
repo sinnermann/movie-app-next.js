7241525d8c6255ff500921957841f58067bcd4e7
sinnerman <sinnerman@gmail.com>
2022-12-03 20:35:25 +0400

    Completion

--- components/Card.js
+++ components/Card.js
@@ -1,23 +1,23 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 
 const Card = ({ result }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div>
+    <div className="p-3 cursor-pointer hover:text-white active:text-yellow-400 xl:hover:scale-105 transition-transform duration-200">
       <Image
         layout="responsive"
         src={BASE_URL + result.backdrop_path}
         width={200}
         height={100}
       />
-      <div className="">
-        <p className="truncate">{result.overview}</p>
-        <h2>{result.title || result.name}</h2>
-        <p>
+      <div className="p-2">
+        <p className="truncate text-lg">{result.overview}</p>
+        <h2 className="text-lg font-bold">{result.title || result.name}</h2>
+        <p className="flex items-center">
           {result.release_date || result.first_air_date}
-          <ThumbUpIcon className="h-5" />
+          <ThumbUpIcon className="h-5 ml-3 mr-2" />
           {result.vote_count}
         </p>
       </div>
